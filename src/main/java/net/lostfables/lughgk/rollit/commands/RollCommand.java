ae0164e9610e7ee75a01ddc461e2bee174464a55
Matthew Hildebrand <korvic@live.ca>
2020-05-02 19:12:50 -0500

    Simplify if statement.

--- src/main/java/net/lostfables/lughgk/rollit/commands/RollCommand.java
+++ src/main/java/net/lostfables/lughgk/rollit/commands/RollCommand.java
@@ -25,7 +25,7 @@ public class RollCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length > 1 || args.length < 1) {
+        if(args.length != 1) {
             sender.sendMessage(ChatColor.RED + "[Rollit] Incorrect Arguments for this Command!");
             return false;
         }
